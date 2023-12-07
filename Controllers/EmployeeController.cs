a019fd345d2dd746acb4d10fea920cdab62c3550
Justin Haddock <jhadddock@protonmail.com>
2023-12-07 03:12:42 -0800

    Add placeholder detail action

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Employee.Controllers;
+
+public class EmployeeController : Controller
+{
+	// 
+	// GET: /Employee/
+	public string Index()
+	{
+		return "This is my default action...";
+	}	
+	// 
+	// GET: /Employee/Detail/ 
+	public string Detail(string ID)
+	{
+		return HtmlEncoder.Default.Encode($"Employee ID is: {ID}");
+
+	}
+}
