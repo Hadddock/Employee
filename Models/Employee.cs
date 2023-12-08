b57c9858227c8a92d17d240c692b579b3ecfd414
Justin Haddock <jhadddock@protonmail.com>
2023-12-07 17:58:08 -0800

    Add Employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Employees
+{
+	public class Employee
+	{
+
+		[BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+		[BsonElement("first_name")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+		[BsonElement("last_name")]
+		public string LastName { get; set; }
+		[DataType(DataType.Date)]
+		[BsonElement("hire_date")]
+		public DateTime HireDate { get; set; }
+		[DataType(DataType.Date)]
+		[BsonElement("date_of_birth")]
+		public DateTime DateOfBirth { get; set; }
+		[BsonElement("email")]
+		public string Email {  get; set; }
+		[BsonElement("phone")]
+		public string Phone { get; set; }
+		[BsonElement("is_administrator")]
+		public bool IsAdministrator { get; set; }
+		[BsonElement("salary")]
+		[BsonRepresentation(BsonType.Decimal128)]
+		public decimal Salary {  get; set; }	
+	}
+
+	
+
+	
+}
