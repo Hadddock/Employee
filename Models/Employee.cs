273a1de5605326dba2add846ca989fbd737184ca
Justin Haddock <jhadddock@protonmail.com>
2023-12-07 18:44:25 -0800

    Add display names

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace Employees
@@ -11,21 +12,27 @@ namespace Employees
         [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
         public string Id { get; set; }
 		[BsonElement("first_name")]
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
-		[BsonElement("last_name")]
+        [DisplayName("Last Name")]
+        [BsonElement("last_name")]
 		public string LastName { get; set; }
 		[DataType(DataType.Date)]
-		[BsonElement("hire_date")]
+        [DisplayName("Hire Date")]
+        [BsonElement("hire_date")]
 		public DateTime HireDate { get; set; }
 		[DataType(DataType.Date)]
-		[BsonElement("date_of_birth")]
+        [DisplayName("Date of Birth")]
+        [BsonElement("date_of_birth")]
 		public DateTime DateOfBirth { get; set; }
-		[BsonElement("email")]
+        [DisplayName("Date of Birth")]
+        [BsonElement("email")]
 		public string Email {  get; set; }
 		[BsonElement("phone")]
 		public string Phone { get; set; }
 		[BsonElement("is_administrator")]
-		public bool IsAdministrator { get; set; }
+        [DisplayName("Administrator Privileges")]
+        public bool IsAdministrator { get; set; }
 		[BsonElement("salary")]
 		[BsonRepresentation(BsonType.Decimal128)]
 		public decimal Salary {  get; set; }	
