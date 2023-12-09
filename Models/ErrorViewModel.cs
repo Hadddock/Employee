c87a4b146f9f0a2d6926e3c17fdaf87afc689301
Justin Haddock <jhadddock@protonmail.com>
2023-12-08 18:14:12 -0800

    Display exceptions in Error page

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -3,6 +3,7 @@ namespace Employee.Models
 	public class ErrorViewModel
 	{
 		public string? RequestId { get; set; }
+		public Exception? Ex { get; set; }
 
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 	}
